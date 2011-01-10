2f3564aace54b737408f8414e32e182c29db3a42
preludedrew <preludedrew@gmail.com>
2011-01-09 23:49:26 -0700

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,111 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/htc/heroc/BoardConfigVendor.mk
+
+TARGET_NO_BOOTLOADER := true
+
+TARGET_BOARD_PLATFORM := msm7k
+TARGET_BOARD_PLATFORM_GPU := qcom
+
+# ARMv6-compatible processor rev 5 (v6l)
+TARGET_CPU_ABI := armeabi-v6j
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv6j
+TARGET_BOOTLOADER_BOARD_NAME := heroc
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := CUSTOM
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := libWifiApi
+BOARD_WLAN_TI_STA_DK_ROOT   := system/wlan/ti/sta_dk_4_0_4_32
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_ARG      := ""
+WIFI_DRIVER_MODULE_NAME     := "wlan"
+WIFI_FIRMWARE_LOADER        := "wlan_loader"
+
+BOARD_USES_GENERIC_AUDIO := false
+
+# Use HTC USB Function Switch to enable tethering via USB
+BOARD_USE_HTC_USB_FUNCTION_SWITCH := true
+
+BOARD_USE_HTC_LIBSENSORS := true
+BOARD_USE_HERO_LIBSENSORS := true
+BOARD_USES_QCOM_LIBS := true
+
+BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
+BOARD_KERNEL_BASE := 0x19200000
+
+BOARD_HAVE_BLUETOOTH := true
+
+BOARD_EGL_CFG := device/htc/heroc/egl.cfg
+
+BOARD_VENDOR_USE_AKMD := akm8973
+
+BOARD_VENDOR_QCOM_AMSS_VERSION := 6355
+
+BOARD_USES_QCOM_LIBS := true
+
+#BOARD_USES_ECLAIR_LIBCAMERA := true
+
+BOARD_USE_FROYO_LIBCAMERA := true
+
+# Use RGB565 surfaces until userspace drivers are upgraded
+BOARD_NO_RGBX_8888 := true
+
+# use old sensors HAL
+TARGET_USES_OLD_LIBSENSORS_HAL := true
+
+# # cat /proc/mtd
+# dev:    size   erasesize  name
+# mtd0: 000a0000 00020000 "misc"
+# mtd1: 00420000 00020000 "recovery"
+# mtd2: 002c0000 00020000 "boot"
+# mtd3: 0f000000 00020000 "system"
+# mtd4: 05000000 00020000 "cache"
+# mtd5: 09120000 00020000 "userdata"
+
+#OLD
+#BOARD_BOOTIMAGE_PARTITION_SIZE := 002c0000
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00420000
+#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0f000000
+#BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x01920000
+
+#NEW FROM GSM HERO
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00280000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0aa00000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0a5c0000
+
+
+# The size of a block that can be marked bad.
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+#TARGET_RECOVERY_UI_LIB := librecovery_ui_heroc librecovery_ui_generic
+
+TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
+
+PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=\$(TOP)/device/htc/heroc/prelink-linux-arm-heroc.map
+TARGET_PREBUILT_KERNEL := device/htc/heroc/kernel
---
+++ heroc.mk
@@ -0,0 +1,117 @@
+#
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# This is the product configuration for a generic GSM passion,
+# not specialized for any geography.
+#
+
+
+PRODUCT_COPY_FILES += \
+    device/htc/heroc/init.heroc.rc:root/init.heroc.rc
+
+
+$(call inherit-product-if-exists, vendor/htc/heroc/heroc-vendor.mk)
+
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.com.google.clientidbase=android-sprint-us \
+	ro.com.google.locationfeatures=1 \
+	ro.cdma.home.operator.numeric=310120 \
+	ro.cdma.home.operator.alpha=Sprint \
+	ro.setupwizard.enable_bypass=1 \
+	ro.media.dec.jpeg.memcap=20000000 \
+	dalvik.vm.lockprof.threshold=500 \
+	dalvik.vm.dexopt-flags=m=y
+
+DEVICE_PACKAGE_OVERLAYS += device/htc/heroc/overlay
+
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
+
+# media config xml file
+PRODUCT_COPY_FILES += \
+    device/htc/heroc/media_profiles.xml:system/etc/media_profiles.xml
+
+PRODUCT_PACKAGES += \
+    librs_jni \
+    sensors.heroc \
+    lights.heroc
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    device/htc/heroc/heroc-keypad.kl:system/usr/keylayout/heroc-keypad.kl \
+    device/htc/heroc/heroc-keypad.kcm.bin:system/usr/keychars/heroc-keypad.kcm.bin \
+    device/htc/heroc/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl
+
+# Passion uses high-density artwork where available
+PRODUCT_LOCALES += mdpi
+
+PRODUCT_COPY_FILES += \
+    device/htc/heroc/vold.fstab:system/etc/vold.fstab \
+    device/htc/heroc/gps.conf:system/etc/gps.conf \
+    device/htc/heroc/apns-conf.xml:system/etc/apns-conf.xml
+
+PRODUCT_COPY_FILES += \
+   device/htc/heroc/wlan.ko:system/lib/modules/wlan.ko \
+   device/htc/heroc/modules/ah6.ko:system/lib/modules/2.6.29.6-cyanogenmod/ah6.ko \
+   device/htc/heroc/modules/deflate.ko:system/lib/modules/2.6.29.6-cyanogenmod/deflate.ko \
+   device/htc/heroc/modules/esp6.ko:system/lib/modules/2.6.29.6-cyanogenmod/esp6.ko \
+   device/htc/heroc/modules/hid-dummy.ko:system/lib/modules/2.6.29.6-cyanogenmod/hid-dummy.ko \
+   device/htc/heroc/modules/ip6_tunnel.ko:system/lib/modules/2.6.29.6-cyanogenmod/ip6_tunnel.ko \
+   device/htc/heroc/modules/ipcomp6.ko:system/lib/modules/2.6.29.6-cyanogenmod/ipcomp6.ko \
+   device/htc/heroc/modules/ipv6.ko:system/lib/modules/2.6.29.6-cyanogenmod/ipv6.ko \
+   device/htc/heroc/modules/mip6.ko:system/lib/modules/2.6.29.6-cyanogenmod/mip6.ko \
+   device/htc/heroc/modules/ramzswap.ko:system/lib/modules/2.6.29.6-cyanogenmod/ramzswap.ko \
+   device/htc/heroc/modules/sit.ko:system/lib/modules/2.6.29.6-cyanogenmod/sit.ko \
+   device/htc/heroc/modules/tunnel4.ko:system/lib/modules/2.6.29.6-cyanogenmod/tunnel4.ko \
+   device/htc/heroc/modules/tunnel6.ko:system/lib/modules/2.6.29.6-cyanogenmod/tunnel6.ko \
+   device/htc/heroc/modules/xfrm6_mode_beet.ko:system/lib/modules/2.6.29.6-cyanogenmod/xfrm6_mode_beet.ko \
+   device/htc/heroc/modules/xfrm6_mode_ro.ko:system/lib/modules/2.6.29.6-cyanogenmod/xfrm6_mode_ro.ko \
+   device/htc/heroc/modules/xfrm6_mode_transport.ko:system/lib/modules/2.6.29.6-cyanogenmod/xfrm6_mode_transport.ko \
+   device/htc/heroc/modules/xfrm6_mode_tunnel.ko:system/lib/modules/2.6.29.6-cyanogenmod/xfrm6_mode_tunnel.ko \
+   device/htc/heroc/modules/xfrm6_tunnel.ko:system/lib/modules/2.6.29.6-cyanogenmod/xfrm6_tunnel.ko \
+   device/htc/heroc/modules/xfrm_ipcomp.ko:system/lib/modules/2.6.29.6-cyanogenmod/xfrm_ipcomp.ko \
+   device/htc/heroc/modules/xt_TCPMSS.ko:system/lib/modules/2.6.29.6-cyanogenmod/xt_TCPMSS.ko \
+   device/htc/heroc/modules/xt_hashlimit.ko:system/lib/modules/2.6.29.6-cyanogenmod/xt_hashlimit.ko 
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/htc/heroc/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+
+$(call inherit-product-if-exists, vendor/htc/heroc/heroc-vendor.mk)
+
+# stuff common to all HTC phones
+$(call inherit-product, device/htc/common/common.mk)
+
+$(call inherit-product, build/target/product/full_base.mk)
+
+
+PRODUCT_NAME := htc_heroc
+PRODUCT_DEVICE := heroc
