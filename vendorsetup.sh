f47c267a7d552ed32473a47654eaad95bc0c2080
andrew.boren <andrew.boren@gmail.com>
2011-03-01 21:10:27 -0700

    heroc: Remove non-evervolv build options.
    Change-Id: Ic6ce944511e601d40e0db79101ff943051cf80fa

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo full_heroc-userdebug
-add_lunch_combo full_heroc-eng
+#We're only going to be building Evervolv, so comment this out for now
+#add_lunch_combo full_heroc-userdebug
+#add_lunch_combo full_heroc-eng
