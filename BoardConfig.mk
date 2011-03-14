bdf53c30f06b6850fdc27c5cfc952429a7a1d1b1
Decad3nce <Decad3nce@tweaktool.com>
2011-03-14 13:22:52 -0500

    heroc: Add BOARD_AVOID_DRAW_TEXTURE_EXTENSION for improved 2D rendering for mdpi device
    Change-Id: If92998dfd383e9b2a60c6b41929ee7f084a52489

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,9 @@ WIFI_FIRMWARE_LOADER        := "wlan_loader"
 BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
 BOARD_KERNEL_BASE := 0x19200000
 
+#libsurfaceflinger to avoid Draw Texture Extenstion
+BOARD_AVOID_DRAW_TEXTURE_EXTENSION := true
+
 BOARD_USES_GENERIC_AUDIO := false
 
 # Use HTC USB Function Switch to enable tethering via USB
