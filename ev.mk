005e9b1ce9f877c1ae58258f8a96b67d273eefda
wjb <jaybob413@gmail.com>
2012-03-09 11:38:19 -0500

    use ICS as PRODUCT_CODENAME for now
    Change-Id: I280d910867070d52c29c68b5d0cce393d69007d6

--- ev.mk
+++ ev.mk
@@ -15,7 +15,7 @@ PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_ID=IML74K BUILD_FINGERPRINT=google/soju/crespo:4.0.3/IML74K/239410:user/release-keys PRIVATE_BUILD_DESC="soju-user 4.0.3 IML74K 239410 release-keys" BUILD_NUMBER=239410
 
 # Set up the product codename, build version & MOTD.
-# PRODUCT_CODENAME := Eligo
+PRODUCT_CODENAME := ICS
 # PRODUCT_VERSION_DEVICE_SPECIFIC := p1
 
 #PRODUCT_MOTD :="\n\n\n------------MESSAGE------------\nThank you for choosing Evervolv\n for your HTC Droid Eris!\n\nPlease visit us at:\n \#evervolv on irc.freenode.net\n\nFollow @preludedrew on twitter\n for the latest Evervolv updates\n\nGet the latest rom at:\n http://www.evervolv.com\n--------------------------------\n"
